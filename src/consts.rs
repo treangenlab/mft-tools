4fb5be0a9c9459551a1ac976130b32fd99b36ec8
Ryan Doughty <rdd4@p00.cs.rice.edu>
2026-04-24 09:19:31 -0500

    update ascii characters length

--- src/consts.rs
+++ src/consts.rs
@@ -1,2 +1,2 @@
 pub const MFT_VERSION: &str = env!("CARGO_PKG_VERSION");
-pub const MAP_ALPHABET: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+pub const MAP_ALPHABET: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
